e0881549b3cd0aa37161e99f9f2542fd4d1e2696
shivamvora <shivampatel27102001@gmail.com>
2021-03-12 19:27:37 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import "bootswatch/dist/materia/bootstrap.min.css";
 import Languages from './components/Languages';
 import InputHook from './components/InputHook';
+import Counter from './components/useEffect/Counter';
 
 
 function App() {
   return (
     <div className="container mt-5  ">
       <Languages/>
-      
+      <Counter/>
     
     </div>
   );
